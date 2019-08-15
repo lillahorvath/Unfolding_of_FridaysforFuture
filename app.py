e4d2ef0f147849898cde5777ca87d392064427ec
lilla_horvath <lll.hrvth@gmail.com>
2019-08-15 14:16:45 -0400

    correct var names

--- app.py
+++ app.py
@@ -9,53 +9,53 @@ from fff_map import map_data, map_create
 from fff_twitter import tw_daily_data, tw_daily_create, tw_hourly_data, tw_hourly_create, tw_nlp_data, tw_nlp_create
 # ----------------------------------------------------------
 
-# initialize tell-me-about-fff
-# ----------------------------
-tellmetaboutfff = Flask(__name__)
+# initialize tell me about fff app
+# --------------------------------
+app = Flask(__name__)
 
 # take me to...
 # -------------
-@tellmetaboutfff.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def page1():
 	return render_template('one.html')
 
-@tellmetaboutfff.route('/two', methods=['GET'])
+@app.route('/two', methods=['GET'])
 def page2():
     return render_template('two.html')
 
-@tellmetaboutfff.route('/three', methods=['GET'])
+@app.route('/three', methods=['GET'])
 def page3():
 	return render_template('three.html', resources=CDN.render())
 
-@tellmetaboutfff.route('/four', methods=['GET'])
+@app.route('/four', methods=['GET'])
 def page4():
 	return render_template('four.html')
 
-@tellmetaboutfff.route('/five', methods=['GET'])
+@app.route('/five', methods=['GET'])
 def page5():
 	return render_template('five.html', resources=CDN.render())	
 
-@tellmetaboutfff.route('/six', methods=['GET'])
+@app.route('/six', methods=['GET'])
 def page6():
 	return render_template('six.html', resources=CDN.render())
 
-@tellmetaboutfff.route('/seven', methods=['GET'])
+@app.route('/seven', methods=['GET'])
 def page7():
 	return render_template('seven.html', resources=CDN.render())	
 
-@tellmetaboutfff.route('/eight', methods=['GET'])
+@app.route('/eight', methods=['GET'])
 def page8():
 
 	return render_template('eight.html', resources=CDN.render())
 
-@tellmetaboutfff.route('/nine', methods=['GET'])
+@app.route('/nine', methods=['GET'])
 def page9():
 
 	return render_template('nine.html', resources=CDN.render())		
 
 # create visualizations to embed 
 # ------------------------------
-@tellmetaboutfff.route('/plot_map')
+@app.route('/plot_map')
 def create_map():
 
 	prot_pop_geo, prot_pop = map_data()
@@ -63,7 +63,7 @@ def create_map():
 
 	return simplejson.dumps(json_item(maptabs, "map"))
 
-@tellmetaboutfff.route('/plot_tw_daily')
+@app.route('/plot_tw_daily')
 def create_twd():
 
 	daily_count_df, daily_count_avg_df = tw_daily_data()
@@ -71,7 +71,7 @@ def create_twd():
 
 	return simplejson.dumps(json_item(tw_daily, "tw_daily"))
 
-@tellmetaboutfff.route('/plot_tw_hourly')
+@app.route('/plot_tw_hourly')
 def create_twh():
 
 	hourly_count_avg_df = tw_hourly_data()
@@ -79,7 +79,7 @@ def create_twh():
 
 	return simplejson.dumps(json_item(tw_hourly, "tw_hourly"))
 
-@tellmetaboutfff.route('/plot_tw_nlp')
+@app.route('/plot_tw_nlp')
 def create_twn():
 
 	words_p, pop_p, pop_p_unique, words_np, pop_np, pop_np_unique = tw_nlp_data()
